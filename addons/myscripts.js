764428114db6fdf728daa4f74c25af1991f1a093
Richard Pastenes <richard@pastenes.com>
2014-04-07 16:02:32 -0700

    More JS commands for console.

--- addons/myscripts.js
+++ addons/myscripts.js
@@ -1,2 +1,13 @@
-console.dir(document.querySelector("#main"));
-console.warn("Fatal error!!");
+console.group("Page links");
+console.dir(document.querySelectorAll('a'));
+console.groupEnd();
+
+console.groupCollapsed("Tables");
+console.dir(document.querySelectorAll('tr'));
+console.groupEnd();
+
+console.time("Bigloop");
+	for (var i = 1000000 - 1; i >= 0; i-- ) {
+
+	};
+console.timeEnd("Bigloop");
